aae6c65573fc2e686eafb8f9b53e27d554d55a69
Anatoly Pulyaevskiy <anatoly.pulyaevskiy@gmail.com>
2017-07-16 17:42:31 +0300

    Sleep between deploys

--- tool/deploy.sh
+++ tool/deploy.sh
@@ -13,6 +13,8 @@ pub build bin
 cp build/bin/clear.dart.js index.js
 firebase deploy --only functions --token "$FIREBASE_TOKEN"
 
+sleep 5
+
 # Create new functions
 cp build/bin/index.dart.js index.js
 firebase deploy --only functions --token "$FIREBASE_TOKEN"
