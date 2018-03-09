a52d622b2cd4691e4d664a2dac68aacb9fed9ae5
Anatoly Pulyaevskiy <anatoly.pulyaevskiy@gmail.com>
2018-03-09 11:18:01 -0800

    Removed deleting existing functions on deploy

--- tool/deploy.sh
+++ tool/deploy.sh
@@ -11,15 +11,8 @@ npm run build
 
 if [ -z "$TRAVIS" ]; then
     echo "Provisioning functions for local environment"
-    # The only way I know to delete existing functions is to deploy empty module:
-    cp build/node/index.dart.js build/node/index.dart.js.temp
-    cp build/node/clear.dart.js build/node/index.dart.js
-    firebase deploy --only functions
-
     firebase functions:config:set someservice.key=123456 someservice.url="https://example.com" someservice.enabled=true
-
     # Create new functions
-    cp build/node/index.dart.js.temp build/node/index.dart.js
     firebase deploy --only functions
 else
     echo "Provisioning functions for Travis build environment"
@@ -32,14 +25,8 @@ else
     gcloud version
     gcloud auth activate-service-account --key-file "$FIREBASE_SERVICE_ACCOUNT_FILEPATH"
 
-    # The only way I know to delete existing functions is to deploy empty module:
-    cp build/node/index.dart.js build/node/index.dart.js.temp
-    cp build/node/clear.dart.js build/node/index.dart.js
-    firebase deploy --only functions --token "$FIREBASE_TOKEN" --project "$FIREBASE_PROJECT_ID"
-
     firebase functions:config:set --token="$FIREBASE_TOKEN" --project "$FIREBASE_PROJECT_ID" someservice.key=123456 someservice.url="https://example.com" someservice.enabled=true
 
     # Create new functions
-    cp build/node/index.dart.js.temp build/node/index.dart.js
     firebase deploy --only functions --token "$FIREBASE_TOKEN" --project "$FIREBASE_PROJECT_ID"
 fi
