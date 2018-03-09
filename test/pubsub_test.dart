8bf7bb35ac0d76e0dde8edde194b54177048215d
Anatoly Pulyaevskiy <anatoly.pulyaevskiy@gmail.com>
2018-03-08 23:28:28 -0800

    WIP on pubsub test

---
+++ test/pubsub_test.dart
@@ -0,0 +1,48 @@
+// Copyright (c) 2018, Anatoly Pulyaevskiy. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+@TestOn('node')
+import 'dart:async';
+import 'dart:js';
+
+import 'package:firebase_admin_interop/firebase_admin_interop.dart';
+import 'package:node_interop/child_process.dart';
+import 'package:test/test.dart';
+
+import 'setup_admin.dart';
+
+void main() {
+  App app = initFirebaseApp();
+
+  group('HTTPS', () {
+    tearDownAll(() async {
+      await app.delete();
+    });
+
+    test('save to database', () async {
+      var payload = new DateTime.now().toUtc().toIso8601String();
+      var command =
+          'gcloud beta pubsub topics publish testTopic \'{"payload":"$payload"}\'';
+      var exitCode = await exec(command);
+      expect(exitCode, 0);
+
+      var snapshot = await app
+          .database()
+          .ref('/tests/pubsubToDatabase')
+          .once<String>('value');
+      expect(snapshot.val(), payload);
+    });
+  });
+}
+
+Future<int> exec(String command) {
+  Completer<int> completer = new Completer<int>();
+  childProcess.exec(command, new ExecOptions(),
+      allowInterop((error, stdout, stderr) {
+    int result = (error == null) ? 0 : error.code;
+    print(stdout);
+    print(stderr);
+    completer.complete(result);
+  }));
+  return completer.future;
+}
